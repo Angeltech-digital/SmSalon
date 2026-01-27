c5aed319de5682a77244cb3272c5ea5b95d249fb
Angela-G9 <angelagathoni9568@gmail.com>
2026-01-27 19:05:14 +0300

    updated

--- Backend/salon_project/wsgi.py
+++ Backend/salon_project/wsgi.py
@@ -3,9 +3,12 @@ WSGI config for salon_project.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # <-- add this
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salon_project.settings')
 
 application = get_wsgi_application()
+
+# Serve static files with WhiteNoise
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
