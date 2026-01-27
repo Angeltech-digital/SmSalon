fdda167048a9d7fd061c78c1a5f9633377f5a625
Angela-G9 <angelagathoni9568@gmail.com>
2026-01-27 18:28:33 +0300

    updated settings.py

--- Backend/salon_project/settings.py
+++ Backend/salon_project/settings.py
@@ -184,17 +184,10 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 # Logging
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-    },
+    'disable_existing_loggers': True,
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
         },
     },
     'root': {
