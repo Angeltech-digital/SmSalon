adc635ecf04b885de970a68bec5dd204162a7fe9
Angela-G9 <angelagathoni9568@gmail.com>
2026-01-28 20:00:19 +0300

    updated

--- Backend/salon_project/settings.py
+++ Backend/salon_project/settings.py
@@ -126,12 +126,16 @@ CORS_ALLOWED_ORIGINS = [
     "https://smsalon-ehqso.ondigitalocean.app",
     "http://0.0.0.0:8001",
     "http://localhost:8001",
+    "http://127.0.0.1:8001"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://smsalon-ehqso.ondigitalocean.app",
     "http://0.0.0.0:8001",
     "http://localhost:8001",
+    "http://127.0.0.1:8001"
+
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
